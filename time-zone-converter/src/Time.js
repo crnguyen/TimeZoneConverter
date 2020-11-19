670d93b3ca3b20a432713e8b5b9140d2b6b2cf7e
Cristina Nguyen <c.nguyen0994@gmail.com>
2020-11-18 21:07:02 -0800

    set up Time component

---
+++ time-zone-converter/src/Time.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+// The United States uses nine standard time zones. 
+// From east to west they are Atlantic Standard Time (AST), 
+// Eastern Standard Time (EST), 
+// Central Standard Time (CST), 
+// Mountain Standard Time (MST), 
+// Pacific Standard Time (PST), 
+// Alaskan Standard Time (AKST), 
+// Hawaii-Aleutian Standard Time (HST), 
+// Samoa standard time (UTC-11)
+// Chamorro Standard Time (UTC+10).
+
+const Time = (props) => {
+    return(
+        <div>
+
+
+        </div>
+    )
+}
